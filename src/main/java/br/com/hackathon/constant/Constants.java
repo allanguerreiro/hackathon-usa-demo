c59baac365da5c1a94619f4f1a5ed06d7ac70d1f
allanguerreiro <allanguerreiro@gmail.com>
2022-09-11 09:27:51 -0300

    Cleaning Constants.java

--- src/main/java/br/com/hackathon/constant/Constants.java
+++ src/main/java/br/com/hackathon/constant/Constants.java
@@ -10,7 +10,4 @@ public class Constants {
     public static final String API_DEPLOY_CONTRACT = "/api/send/transaction";
     public static final String GENERIC_EXCEPTION = "Exception encountered!";
     public static final String PLEASE_SUPPLY_REAL_DATA = "Please Supply Real Data!";
-
-    public static final String DEFAULT_ADDRESS = "0x281055afc982d96fab65b3a49cac8b878184cb16";
-    public static final String DEFAULT_CONTRACT_ADDRESS = "00000000000000000000";
 }
