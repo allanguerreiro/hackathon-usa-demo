47756489b7f18974d65902a84b8feb51d17cbcde
allanguerreiro <allanguerreiro@gmail.com>
2022-09-11 11:30:20 -0300

    Perform Transfer funds

--- src/main/java/br/com/hackathon/constant/Constants.java
+++ src/main/java/br/com/hackathon/constant/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String API_SEND_TRANSACTION = "/api/send/transaction";
     public static final String API_DEPLOY_CONTRACT_DEVELOPER = "/api/deploy/contract/developer";
     public static final String API_DEPLOY_CONTRACT_MANAGER = "/api/deploy/contract/manager";
-    public static final String API_PERFORM_TRANSFER = "/api/deploy/perform/transfer";
+    public static final String API_PERFORM_TRANSFER = "/api/perform/transfer";
     public static final String GENERIC_EXCEPTION = "Exception encountered!";
     public static final String PLEASE_SUPPLY_REAL_DATA = "Please Supply Real Data!";
 }
